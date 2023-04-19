f46ccbf8cce395a93e2fb4950c5c17e45493ace7
kingtvs12 <122884145+kingtvs12@users.noreply.github.com>
2023-04-19 21:21:55 +0300

    Add files via upload

---
+++ NewsPaper/new/views.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+
+class PostsList(ListView):
+    model = Post
+    ordering = '-time_in'
+    template_name = 'posts.html'
+    context_object_name = 'posts'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context['time_now'] = datetime.utcnow()
+        # context['text'] = None
+        return context
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context['time_now'] = datetime.utcnow()
+        # context['text'] = None
+        return context
